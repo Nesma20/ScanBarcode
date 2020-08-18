c4d3cd436bc1c2b50de239b64327fbe929b19e79
Nesma20 <nesmatharwat20@gmail.com>
2020-08-18 09:54:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TestScanBarcode.kt
@@ -0,0 +1,153 @@
+package com.example.myapplication
+
+import android.animation.AnimatorSet
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.common.internal.Objects
+import com.google.android.material.chip.Chip
+import com.google.mlkit.md.barcodedetection.BarcodeField
+import com.google.mlkit.md.barcodedetection.BarcodeProcessor
+import com.google.mlkit.md.barcodedetection.BarcodeResultFragment
+import com.google.mlkit.md.camera.CameraSource
+import com.google.mlkit.md.camera.CameraSourcePreview
+import com.google.mlkit.md.camera.GraphicOverlay
+import com.google.mlkit.md.camera.WorkflowModel
+import java.io.IOException
+import java.util.ArrayList
+
+class TestScanBarcode : Fragment() {
+    private var cameraSource: CameraSource? = null
+    private var preview: CameraSourcePreview? = null
+    private var graphicOverlay: GraphicOverlay? = null
+    private var settingsButton: View? = null
+    private var flashButton: View? = null
+    private var promptChip: Chip? = null
+    private var promptChipAnimator: AnimatorSet? = null
+    private var workflowModel: WorkflowModel? = null
+    private var currentWorkflowState: WorkflowModel.WorkflowState? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = layoutInflater.inflate(R.layout.fragment_test_scan_barcode, container,false)
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+    override fun onResume() {
+        super.onResume()
+
+        workflowModel?.markCameraFrozen()
+        settingsButton?.isEnabled = true
+        currentWorkflowState = WorkflowModel.WorkflowState.NOT_STARTED
+        cameraSource?.setFrameProcessor(BarcodeProcessor(graphicOverlay!!, workflowModel!!))
+        workflowModel?.setWorkflowState(WorkflowModel.WorkflowState.DETECTING)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        currentWorkflowState = WorkflowModel.WorkflowState.NOT_STARTED
+        stopCameraPreview()
+    }
+    private fun startCameraPreview() {
+        val workflowModel = this.workflowModel ?: return
+        val cameraSource = this.cameraSource ?: return
+        if (!workflowModel.isCameraLive) {
+            try {
+                workflowModel.markCameraLive()
+                preview?.start(cameraSource)
+            } catch (e: IOException) {
+                Log.e(LiveBarcodeScanningActivity.TAG, "Failed to start camera preview!", e)
+                cameraSource.release()
+                this.cameraSource = null
+            }
+        }
+    }
+
+    private fun stopCameraPreview() {
+        val workflowModel = this.workflowModel ?: return
+        if (workflowModel.isCameraLive) {
+            workflowModel.markCameraFrozen()
+            flashButton?.isSelected = false
+            preview?.stop()
+        }
+    }
+
+    private fun setUpWorkflowModel() {
+        workflowModel = ViewModelProviders.of(this).get(WorkflowModel::class.java)
+
+        // Observes the workflow state changes, if happens, update the overlay view indicators and
+        // camera preview state.
+        workflowModel!!.workflowState.observe(this, Observer { workflowState ->
+            if (workflowState == null || Objects.equal(currentWorkflowState, workflowState)) {
+                return@Observer
+            }
+
+            currentWorkflowState = workflowState
+            Log.d(LiveBarcodeScanningActivity.TAG, "Current workflow state: ${currentWorkflowState!!.name}")
+
+            val wasPromptChipGone = promptChip?.visibility == View.GONE
+
+            when (workflowState) {
+                WorkflowModel.WorkflowState.DETECTING -> {
+                    promptChip?.visibility = View.VISIBLE
+                    promptChip?.setText(R.string.prompt_point_at_a_barcode)
+                    startCameraPreview()
+                }
+                WorkflowModel.WorkflowState.CONFIRMING -> {
+                    promptChip?.visibility = View.VISIBLE
+                    promptChip?.setText(R.string.prompt_move_camera_closer)
+                    startCameraPreview()
+                }
+                WorkflowModel.WorkflowState.SEARCHING -> {
+                    promptChip?.visibility = View.VISIBLE
+                    promptChip?.setText(R.string.prompt_searching)
+                    stopCameraPreview()
+                }
+                WorkflowModel.WorkflowState.DETECTED, WorkflowModel.WorkflowState.SEARCHED -> {
+                    promptChip?.visibility = View.GONE
+                    stopCameraPreview()
+                }
+                else -> promptChip?.visibility = View.GONE
+            }
+
+            val shouldPlayPromptChipEnteringAnimation = wasPromptChipGone && promptChip?.visibility == View.VISIBLE
+            promptChipAnimator?.let {
+                if (shouldPlayPromptChipEnteringAnimation && !it.isRunning) it.start()
+            }
+        })
+
+        workflowModel?.detectedBarcode?.observe(this, Observer { barcode ->
+            if (barcode != null) {
+                val barcodeFieldList = ArrayList<BarcodeField>()
+                barcodeFieldList.add(BarcodeField("Raw Value", barcode.rawValue ?: ""))
+                BarcodeResultFragment.show(requireActivity().supportFragmentManager, barcodeFieldList)
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraSource?.release()
+        cameraSource = null
+    }
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TestScanBarcode().apply {
+            }
+    }
+}
